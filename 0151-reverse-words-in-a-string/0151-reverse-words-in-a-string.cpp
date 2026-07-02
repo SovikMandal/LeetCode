fe56c792dff3c1c5ced104905dd0a5122524035c
Sovik Mandal <154688869+SovikMandal@users.noreply.github.com>
2026-07-02 14:35:19 +0530

    Time: 0 ms (100%), Space: 10.6 MB (42.78%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string str = "";
+        stack<string> st;
+        for(char ch : s) {
+            if(ch == ' ') {
+                if(!str.empty()) {
+                    st.push(str);
+                    str = "";
+                }
+            } else {
+                str += ch;
+            }
+        }
+
+        if(!str.empty()) st.push(str);
+        string ans = "";
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+            if(!ans.empty()) ans += ' ';
+        }
+
+        ans.pop_back();
+        return ans;
+    }
+};
