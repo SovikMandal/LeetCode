1aeb5f0128054336bc8beea6c1e5bacaf22718a2
Sovik Mandal <154688869+SovikMandal@users.noreply.github.com>
2026-07-02 23:36:32 +0530

    Time: 0 ms (100%), Space: 15.9 MB (5.74%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int index = -1;
+
+        for(int i = n -1; i > 0; i--) {
+            if(nums[i] > nums[i - 1]) {
+                index = i -1;
+                break;
+            }
+        }
+
+        if(index != -1) {
+            int swapIndex = index;
+            for(int j = n - 1; j >= index + 1; j--) {
+                if(nums[j] > nums[index]) {
+                    swapIndex = j;
+                    break;
+                }
+            }
+
+            swap(nums[index], nums[swapIndex]);
+        }
+
+        reverse(nums.begin() + index + 1, nums.end());
+    }
+};
